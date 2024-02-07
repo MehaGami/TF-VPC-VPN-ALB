2e7ee6f3ab5ccba8278102703bb3cd59633943ba
MehaGami <131962243+MehaGami@users.noreply.github.com>
2024-02-07 19:19:15 +0200

    fix

--- ec2/provider.tf
+++ ec2/provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  access_key = "AKIAYIVIHTCYDTMK6WNH"
-  secret_key = "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"
+  access_key = "test"    
+  secret_key = "test"
   region = var.aws_region
   default_tags {
     tags = {
